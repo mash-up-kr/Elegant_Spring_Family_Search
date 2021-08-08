646269c4b4cdc3df88a84059c88f2735e1499c72
kh0712 <kkkh0712@naver.com>
2021-08-08 17:03:09 +0900

    feat: 엘라스틱 서치 Client Config 설정

---
+++ src/main/kotlin/mashup/spring/elegant/search/config/ElasticSearchConfig.kt
@@ -0,0 +1,24 @@
+package mashup.spring.elegant.search.config
+
+
+import org.apache.http.HttpHost
+import org.elasticsearch.client.RestClient
+import org.elasticsearch.client.RestHighLevelClient
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+
+class ElasticSearchConfig (
+    @Value("\${es.url}")
+    private val hostName: String,
+    @Value("\${es.port}")
+    private val port: Int
+){
+    // Elastic Search 와 통신하기 위한 rest client
+    @Bean
+    fun restHighLevelClient() : RestHighLevelClient = RestHighLevelClient(RestClient.builder(HttpHost(hostName, port, "http")))
+
+
+
+
+
+}
