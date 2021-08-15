a56b62552feca87b941ab504aa1439a05f12b24f
gardenLee <khjvvv7@gmail.com>
2021-08-15 22:01:46 +0900

    test: es 접근 테스트

--- src/main/kotlin/mashup/spring/elegant/search/controller/HealthCheckController.kt
+++ src/main/kotlin/mashup/spring/elegant/search/controller/HealthCheckController.kt
@@ -1,16 +1,28 @@
 package mashup.spring.elegant.search.controller
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
 
 @RestController
 class HealthCheckController (
     @Value("\${server.echo}")
-    private val echo : String
+    private val echo : String,
 ){
+
+    @Value("\${es.url}")
+    private val esUrl = "localhost"
 
     @GetMapping("")
     fun healthCheck() = echo
 
+    @GetMapping("/es")
+    fun esHealth(): ResponseEntity<String> {
+        val restTemplate: RestTemplate = RestTemplate()
+
+        return restTemplate.getForEntity(esUrl, String::class.javaObjectType)
+    }
 }
