4ef168c924e974b612efbe2d417bfb5b546f3c06
gardenLee <khjvvv7@gmail.com>
2021-08-15 20:38:43 +0900

    feat: EB 배포 테스트

--- src/main/kotlin/mashup/spring/elegant/search/controller/HealthCheckController.kt
+++ src/main/kotlin/mashup/spring/elegant/search/controller/HealthCheckController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/api/v1/health-check")
 @RestController
-class TestController (
+class HealthCheckController (
     @Value("\${server.echo}")
     private val echo : String
 ){
