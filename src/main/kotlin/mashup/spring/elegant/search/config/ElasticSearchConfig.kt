baf8631e12feaf73d1a2b0afd14f7ea34e268892
gardenLee <khjvvv7@gmail.com>
2021-08-15 23:10:18 +0900

    test: es 접근 테스트

--- src/main/kotlin/mashup/spring/elegant/search/config/ElasticSearchConfig.kt
+++ src/main/kotlin/mashup/spring/elegant/search/config/ElasticSearchConfig.kt
@@ -2,17 +2,16 @@ package mashup.spring.elegant.search.config
 
 
 import mashup.spring.elegant.search.util.getEnvironment
+import org.apache.http.HttpHost
+import org.apache.http.auth.AuthScope
+import org.apache.http.auth.UsernamePasswordCredentials
+import org.apache.http.impl.client.BasicCredentialsProvider
+import org.elasticsearch.client.RestClient
 import org.elasticsearch.client.RestHighLevelClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate
-
-import org.springframework.data.elasticsearch.client.RestClients
-
-import org.springframework.data.elasticsearch.client.ClientConfiguration
-
-
 
 
 @Configuration
@@ -21,23 +20,28 @@ class ElasticSearchConfig (
     private val hostName: String,
     @Value("\${es.port}")
     private val port: Int
-){
-    // Elastic Search 와 통신하기 위한 rest client
-
+) {
     private val username: String = getEnvironment("ES_USERNAME")
     private val password: String = getEnvironment("ES_PASSWORD")
 
     @Bean
-    fun client(): RestHighLevelClient? {
-        val clientConfiguration: ClientConfiguration = ClientConfiguration.builder()
-            .connectedTo("$hostName:$port")
-            .withBasicAuth(username, password)
-            .build()
-        return RestClients.create(clientConfiguration).rest()
-    }
+    fun elasticsearchTemplate() = ElasticsearchRestTemplate(restHighLevelClient())
 
-    @Bean
-    fun elasticsearchTemplate(): ElasticsearchRestTemplate? {
-        return ElasticsearchRestTemplate(client())
+    private fun restHighLevelClient() = RestHighLevelClient(getRestClientBuilder())
+
+    private fun getRestClientBuilder() =
+        RestClient.builder(HttpHost(hostName, port, "https"))
+            .setHttpClientConfigCallback {
+                    httpClientBuilder -> httpClientBuilder
+                .setDefaultCredentialsProvider(getCredentialProvider())
+            }
+
+    private fun getCredentialProvider(): BasicCredentialsProvider {
+        val credentialsProvider = BasicCredentialsProvider()
+        credentialsProvider.setCredentials(
+            AuthScope.ANY,
+            UsernamePasswordCredentials(username, password)
+        )
+        return credentialsProvider
     }
 }
